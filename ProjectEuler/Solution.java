983eea5b6ce103764e57ce9f5184000ae0cd9963
linoox <pd2614g@gmail.com>
2015-09-20 18:44:41 -0400

    euler 10

--- ProjectEuler/Solution.java
+++ ProjectEuler/Solution.java
@@ -108,6 +108,33 @@ public class Solution {
 
 		return 0;
 	}
+	
+	//euler 10
+	 public static int countPrimes(int n) {
+	       
+	       int numOfPrimes = 0;
+	       
+	       boolean[] isPrime = new boolean[n+1];
+	       
+	       for (int i=2; i<=n; i++) {
+	           isPrime[i] = true;
+	       }
+	       
+	       for (int i=2; i*i <= n; i++) {
+	           if (isPrime[i]) {
+	               for (int j=i; i*j <= n; j++) {
+	                   isPrime[i*j]=false;
+	               }
+	           }
+	       }
+	       
+	       for (int i=2; i <= n; i++) {
+	           if(isPrime[i])
+	               numOfPrimes++;
+	       }
+	       
+	       return numOfPrimes;
+	   }
 
 	public static void main(String[] args) {
 
